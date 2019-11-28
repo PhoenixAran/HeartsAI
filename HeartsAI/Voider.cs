b1f2e72e5f383b57977f99f7b145448f09bc96c6
sunnysmiles17 <jadearan.iwnl@gmail.com>
2019-11-28 13:26:07 -0500

    voider++

--- HeartsAI/Voider.cs
+++ HeartsAI/Voider.cs
@@ -63,7 +63,7 @@ namespace HeartsAI
             int idx = -1;
             for ( int i = 0; i < _suits.Length; ++i )
             {
-                if ( _pSuitScores[i] > min )
+                if ( min <  _pSuitScores[i] && 0 < _pSuitScores[i] )
                 {
                     idx = i;
                     min = _pSuitScores[i];
@@ -79,7 +79,7 @@ namespace HeartsAI
             int idx = -1;
             for ( int i = 0; i < _suits.Length; ++i )
             {
-                if ( _pSuitCounts[i] > min )
+                if ( min < _pSuitCounts[i] && 0 < _pSuitCounts[i] )
                 {
                     idx = i;
                     min = _pSuitCounts[i];
@@ -88,6 +88,31 @@ namespace HeartsAI
 
             return _suits[idx];
         }
+
+        private Suit GetMinCountSuit( bool excludeHearts = false )
+        {
+            int max = int.MaxValue;
+            int idx = -1;
+            for ( int i = 0; i < _suits.Length; ++i )
+            {
+                if ( excludeHearts )
+                {
+                    if ( _suits[i] == Suit.Hearts )
+                    {
+                        continue;
+                    }
+                }
+
+                if ( 0 < _pSuitCounts[i] && _pSuitCounts[i] < max )
+                {
+                    idx = i;
+                    max = _pSuitCounts[i];
+                }
+            }
+
+            return _suits[idx];
+        }
+
 
         private void AverageSuitScoresByCounts()
         {
@@ -139,13 +164,13 @@ namespace HeartsAI
                 Hand.Remove( leadCard );
             }
 
-            for ( int i = Hand.Count - 1, j = 0; i >= 0 || j == jLimit; --i)
+            for ( int i = Hand.Count - 1, j = 0; i >= 0 && j < jLimit; --i)
             {
                 var card = Hand[i];
                 if ( card.Suit == maxSuit )
                 {
                     tempList.Add( card );
-                    ++jLimit;
+                    ++j;
                     Hand.RemoveAt( i );
                 }
             }
@@ -191,12 +216,86 @@ namespace HeartsAI
                 return;
             }
 
+            if ( currentTrick.Count > 0 && !HasSuit( currentTrick.LeadSuit ) )
+            {
+                WeighNodeTreeWhereLeadSuitIsVoid( turnNumber, currentTrick );
+                return;
+            }
+
+            if ( currentTrick.Count == 0 )
+            {
+                var excludeHearts = !CanLeadHearts;
+                if ( HandIsAllHeartsAndQueenOfSpades() )
+                {
+                    excludeHearts = false;
+                }
+                var minSuit = GetMinCountSuit( excludeHearts );
+
+                foreach ( var child in children )
+                {
+                    var cardPlayed = child.CardPlayed;
+                    if ( cardPlayed.Suit == Suit.Hearts )
+                    {
+                        child.Weight = -100;
+                    }
+                    else if ( cardPlayed.Suit == minSuit )
+                    {
+                        child.Weight = 100 - cardPlayed.CardRank;
+                    }
+                    else
+                    {
+                        child.Weight = 50 - cardPlayed.CardRank;
+                    }
+                }
+                return;
+            }
+
+            if ( currentTrick.Count > 0 )
+            {
+                var leadSuit = currentTrick.LeadSuit;
+                var minCard = GetMinCardInTrick( currentTrick );
+                foreach( var child in children )
+                {
+                    var cardPlayed = child.CardPlayed;
+                    if ( cardPlayed.Suit != leadSuit )
+                    {
+                        child.Weight = -100;
+                    }
+                    else
+                    {
+                        if ( cardPlayed.CardRank < minCard.CardRank )
+                        {
+                            child.Weight = 100 + ( 14 ) - ( minCard.CardRank - cardPlayed.CardRank );
+                        }
+                        else
+                        {
+                            child.Weight = 100 - ( minCard.CardRank - cardPlayed.CardRank );
+                        }
+                    }
+                }
+                return;
+            }
+
             throw new Exception( "Unexpected Condition in Voider::GetPlayCard" );
         }
 
         private void WeighNodeTreeWhereLeadSuitIsVoid( int turnNumber, Trick currentTrick )
         {
             var children = _current.Children;
+            var minSuit = GetMinCountSuit();
+
+            foreach ( var child in children )
+            {
+                var cardPlayed = child.CardPlayed;
+                if ( cardPlayed.Suit == minSuit )
+                {
+                    child.Weight = cardPlayed.CardRank + 10;
+                }
+                else
+                {
+                    child.Weight = cardPlayed.CardRank;
+                }
+            }
         }
     }
 }
--- HeartsConsole/Program.cs
+++ HeartsConsole/Program.cs
@@ -79,6 +79,76 @@ namespace HeartsConsole
             Console.ReadLine();
         }
 
+        static void Voider()
+        {
+
+            //Set up random players
+            var player1 = new Voider();
+            var player2 = new RandomPlayer();
+            var player3 = new RandomPlayer();
+            var player4 = new RandomPlayer();
+
+            var points = new int[4];
+
+            //Set up game
+            var game = new HeartsGame();
+
+            //Add the players
+            game.Players.AddRange( new Player[] { player1, player2, player3, player4 } );
+
+
+            for ( int i = 0; i < 1000; ++i )
+            {
+                game.PlayRound();
+                for ( int j = 0; j < 4; ++j )
+                {
+                    points[j] += game.Players[j].Points;
+                }
+                game.Reset();
+
+            }
+
+            Console.WriteLine( $"LowLayer: {points[0]}" );
+            Console.WriteLine( $"Player2: {points[1]}" );
+            Console.WriteLine( $"Player3: {points[2]}" );
+            Console.WriteLine( $"Player4: {points[3]}" );
+            Console.ReadLine();
+        }
+
+        static void SplitTest()
+        {
+            //Set up random players
+            var player1 = new Voider();
+            var player2 = new LowLayer();
+            var player3 = new Voider();
+            var player4 = new LowLayer();
+
+            var points = new int[4];
+
+            //Set up game
+            var game = new HeartsGame();
+
+            //Add the players
+            game.Players.AddRange( new Player[] { player1, player2, player3, player4 } );
+
+
+            for ( int i = 0; i < 1000; ++i )
+            {
+                game.PlayRound();
+                for ( int j = 0; j < 4; ++j )
+                {
+                    points[j] += game.Players[j].Points;
+                }
+                game.Reset();
+
+            }
+
+            Console.WriteLine( $"LowLayer1: {points[0]}" );
+            Console.WriteLine( $"Voider1: {points[1]}" );
+            Console.WriteLine( $"LowLayer2: {points[2]}" );
+            Console.WriteLine( $"Voider2: {points[3]}" );
+            Console.ReadLine();
+        }
 
         static void Main( string[] args )
         {
@@ -87,8 +157,7 @@ namespace HeartsConsole
             Pool<Trick>.WarmCache( 100 );
             ListPool<Trick>.WarmCache( 100 );
             ListPool<Card>.WarmCache( 800 );
-            LowLayerTest();
-
+            SplitTest();
             
             
         }
